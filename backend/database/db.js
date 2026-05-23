45ace001405d2c705efea599711d2380f1112281
Prem M <mprem5032@gmail.com>
2026-05-23 11:03:09 +0530

    first commit

--- backend/database/db.js
+++ backend/database/db.js
@@ -5,13 +5,22 @@ dotenv.config();
 
 const { Pool } = pg;
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+export const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      }
+);
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
